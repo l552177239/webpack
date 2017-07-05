b3b851fa940c78b66485300936334941afe67b66
L552177239 <552177239@qq.com>
2017-07-05 22:03:58 +0800

    react

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log(666)
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index'
+
+ReactDOM.render(<div>我是DIV</div>,document.getElementById('root'))
--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,18 @@ module.exports = {
 	entry: './src/index.js',
 	output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js"
-  }
+    filename: "bundle.js",
+    publicPath:"dist/"
+  },
+  devtool:"source-map",
+  module: {
+	  rules: [
+	    { test: /\.js$/, exclude: /node_modules/, use: "babel-loader" },
+	    {
+        test: /\.css$/,
+        use: ["style-loader","css-loader","postcss-loader"]
+      },
+      { test: /\.(jpe?g|png)$/, use: 'file-loader' }
+	  ]
+	}
 }
